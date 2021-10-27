789fb40d388a969bd65729616c4979b2d4e12204
Masa <masa@hashicorp.com>
2021-10-27 14:21:15 +0900

    all ws

--- variables.tf
+++ variables.tf
@@ -2,11 +2,39 @@ variable "ws" {
 	default = [
 		{
 			ws_name = "ProjX-network-dev"
-			tags = [ "dev", "ProjX" ]
+			tags = [ "dev", "network" ]
 		},
 		{
 			ws_name = "ProjX-storage-dev"
-			tags = [ "storage", "ProjX" ]
+			tags = [ "dev", "storage" ]
+		},
+		{
+			ws_name = "ProjX-app-dev"
+			tags = [ "dev",  "app" ]
+		},
+		{
+			ws_name = "ProjX-network-staging"
+			tags = [ "staging", "network" ]
+		},
+		{
+			ws_name = "ProjX-storage-staging"
+			tags = [ "staging", "storage" ]
+		},
+		{
+			ws_name = "ProjX-app-staging"
+			tags = [ "staging",  "app" ]
+		},
+		{
+			ws_name = "ProjX-network-prod"
+			tags = [ "prod", "network" ]
+		},
+		{
+			ws_name = "ProjX-storage-prod"
+			tags = [ "prod", "storage" ]
+		},
+		{
+			ws_name = "ProjX-app-prod"
+			tags = [ "prod",  "app" ]
 		},
 	]
 }
